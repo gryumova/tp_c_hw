fd44ca037f29cccf3329626affe9b90f69d9abf1
Elizaveta <e.gryumova@yandex.ru>
2021-10-10 20:02:09 +0300

    add codecov

--- project/include/task_structure.h
+++ project/include/task_structure.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "../include/read_write.h"
 
 
@@ -9,5 +10,6 @@ TaskList_t* add_elem(Task* a, TaskList_t* head);
 TaskList_t* initialise(Task* elem);
 TaskList_t* insert_task(TaskList_t* head, Task* elem);
 
+const bool date_comparison(Date date1, Date date2);
 int print_task(TaskList_t* head);
 int clear(TaskList_t *head);
--- project/src/read_write.c
+++ project/src/read_write.c
@@ -3,102 +3,89 @@
 #include <stdbool.h>
 
 int write_file(const size_t k, const char filename[], const char action[]) {
-    FILE* database = NULL;
-    if (strcmp(action, "-c") == 0) {
-        database = fopen(filename, "wb");
-    }
+  FILE *database = NULL;
+  if (strcmp(action, "-c") == 0) {
+    database = fopen(filename, "wb");
+  }
 
-    if (strcmp(action, "-a") == 0) {
-        database = fopen(filename, "ab");
-    }
+  if (strcmp(action, "-a") == 0) {
+    database = fopen(filename, "ab");
+  }
 
-    if (database == NULL) {
-        puts("File not found");
-        return(FAILED_FILE_OPENNING);
-    }
+  if (database == NULL) {
+    puts("File not found");
+    return (FAILED_FILE_OPENNING);
+  }
 
-    Task* info = (Task*) calloc(1, sizeof(Task));
-    if (info == NULL) {
-        puts("Memory error");
-        if (fclose(database)) {
-            return FAILED_FILE_CLOSING;
-        }
-        return(FAILED_MEMORY_ALLOCATION);
-    }
-
-    size_t i = 0;
-    while (i < k) {
-        if (fscanf(stdin, "%10lu%2d%30s%2d.%2d.%4d", &info->id_task, &info->priority, info->description, &info->date.day, &info->date.mn, &info->date.year) < 0) {
-            puts("Error getting full data");
-            free(info);
-            if (fclose(database)) {
-                return FAILED_FILE_CLOSING;
-            }
-            return(SCANF_ERROR);
-        }
-        if (strlen(info->description) && info->date.day && info->date.mn && info->date.year) {
-            fwrite(info, sizeof(Task), 1, database);
-        } else {
-            puts("Some data was incomplete and didn't write to a file");
-        }
-        i++;
-    }
-
-    free(info);
+  Task *info = (Task *)calloc(1, sizeof(Task));
+  if (info == NULL) {
+    puts("Memory error");
     if (fclose(database)) {
+      return FAILED_FILE_CLOSING;
+    }
+    return (FAILED_MEMORY_ALLOCATION);
+  }
+
+  size_t i = 0;
+  while (i < k) {
+    if (fscanf(stdin, "%10lu%2d%30s%2d.%2d.%4d", &info->id_task,
+               &info->priority, info->description, &info->date.day,
+               &info->date.mn, &info->date.year) < 0) {
+      puts("Error getting full data");
+      free(info);
+      if (fclose(database)) {
         return FAILED_FILE_CLOSING;
+      }
+      return (SCANF_ERROR);
     }
-    return NO_ERROR;
+    if (strlen(info->description) && info->date.day && info->date.mn &&
+        info->date.year) {
+      fwrite(info, sizeof(Task), 1, database);
+    } else {
+      puts("Some data was incomplete and didn't write to a file");
+    }
+    i++;
+  }
+
+  free(info);
+  if (fclose(database)) {
+    return FAILED_FILE_CLOSING;
+  }
+  return NO_ERROR;
 }
 
+TaskList_t *read_file(const char filename[]) {
+  FILE *database = NULL;
+  database = fopen(filename, "rb");
 
-TaskList_t* read_file(const char filename[]) { 
-    FILE* database = NULL;
-    database = fopen(filename, "rb");
+  if (database == NULL) {
+    puts("File not found");
+    return NULL;
+  }
 
-    if (database == NULL) {
-        puts("File not found");
-                return NULL;
-    }
+  Task *get_data = (Task *)malloc(sizeof(Task));
+  if (get_data == NULL) {
+    puts("Memory error");
+    fclose(database);
+    return NULL;
+  }
 
-    Task* get_data = (Task *)malloc(sizeof(Task));
-    if (get_data == NULL) {
-        puts("Memory error");
-        fclose(database);
-        return NULL;
-    }
+  if (fread(get_data, sizeof(Task), 1, database) == 0) {
+    puts("FILE IS EMPTY");
+    fclose(database);
+    return NULL;
+  }
 
-    if (fread(get_data, sizeof(Task), 1, database) == 0) {
-        puts("FILE IS EMPTY");
-        fclose(database);
-        return NULL;
-    }
+  TaskList_t *head = NULL;
+  head = initialise(get_data);
 
-    TaskList_t* head = NULL;
-    head = initialise(get_data);
+  while (fread(get_data, sizeof(Task), 1, database) != 0) {
+    head = add_elem(get_data, head);
+  }
 
-    while (fread(get_data, sizeof(Task), 1, database) != 0) {
-        head = add_elem(get_data, head);
-    }
-
-    free(get_data);
-    if (fclose(database)) {
-        return NULL;
-    }
-    return head;
-}
-
-
-const bool date_comparison(Date date1, Date date2) {
-    if (date1.year == date2.year && date1.mn == date2.mn){
-        if (date1.day > date2.day)
-            return true;
-    }
-    if (date1.year == date2.year && date1.mn > date2.mn) {
-        return true;
-    }
-    if  (date1.year > date2.year) {
-        return true;
-    }
-    return false;
+  free(get_data);
+  if (fclose(database)) {
+    return NULL;
+  }
+  return head;
 }
