5c68f5a9b1f4aec69f5d79e7cbf24b6e31062f03
Elizaveta <e.gryumova@yandex.ru>
2021-10-11 14:51:50 +0300

    add test

---
+++ build/CMakeFiles/prog.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/elizaveta/Рабочий стол/Технопа/hw/project/src/main.c" "/home/elizaveta/Рабочий стол/Технопа/hw/build/CMakeFiles/prog.dir/project/src/main.c.o"
+  "/home/elizaveta/Рабочий стол/Технопа/hw/project/src/read_write.c" "/home/elizaveta/Рабочий стол/Технопа/hw/build/CMakeFiles/prog.dir/project/src/read_write.c.o"
+  "/home/elizaveta/Рабочий стол/Технопа/hw/project/src/task_structure.c" "/home/elizaveta/Рабочий стол/Технопа/hw/build/CMakeFiles/prog.dir/project/src/task_structure.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../project/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
