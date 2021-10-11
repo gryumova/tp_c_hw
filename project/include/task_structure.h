480bc9c1a9fc09e7b372ed1103fa6454ce5fe94c
Elizaveta <e.gryumova@yandex.ru>
2021-10-12 00:34:59 +0300

    fix test

--- project/include/task_structure.h
+++ project/include/task_structure.h
@@ -9,6 +9,7 @@
 TaskList_t* add_elem(Task* new_task, TaskList_t* head);
 TaskList_t* initialise(Task* elem);
 TaskList_t* insert_task(TaskList_t* task, Task* elem);
+TaskList_t *sort_by_date(TaskList_t *head, Task *elem);
 
 const bool date_comparison(Date date1, Date date2);
 int print_task(TaskList_t* head);
